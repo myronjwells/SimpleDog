b467d94b770fbf5c1f99a31c9928cdd9ac875086
mwells <mwells@haystax.com>
2019-09-21 15:25:12 -0400

    Initial Commit

---
+++ SimpleDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleDog
+//
+//  Created by Myron Wells on 9/21/19.
+//  Copyright © 2019 Myron Wells. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
